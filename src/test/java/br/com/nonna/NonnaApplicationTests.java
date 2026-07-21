fec2d6bec4b5b4b3eb741f3615a6e3be11cf44c0
GabrielBdeC <gabrielbdec@gmail.com>
2026-07-20 21:54:17 -0300

    Primeiro commit

---
+++ src/test/java/br/com/nonna/NonnaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nonna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
